04ef50de2d879ce3dfcfbd16368ade99db24b8f1
njrsouza <njrsouza@outlook.com>
2026-05-16 01:02:58 -0300

    feat: integração do Prisma com banco de dados Postgres e criação da tabela Movie

--- backend/src/models/movie-model.ts
+++ backend/src/models/movie-model.ts
@@ -1,8 +1,12 @@
 export interface MovieModel {
-  id: number;
+  id: string;
   title: string;
   synopsis: string;
   genres: string[];
   duration: number;
   url_movie: string;
+  url_poster: string;
+  cast: string[];
+  directors: string[];
+  createdAt: Date;
 }
