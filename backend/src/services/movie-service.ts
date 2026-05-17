505103f061fd8c30b75337e4d501437b60332861
llucasEmanuel <lluukkaass62@gmail.com>
2026-05-17 03:14:47 +0000

    test: add unit tests to MovieMetadata feature

--- backend/src/services/movie-service.ts
+++ backend/src/services/movie-service.ts
@@ -17,14 +17,13 @@ export class MovieService {
   private repository = new MovieRepository();
 
   async getMetadata(id: string) {
-    const movie = await this.repository.findById(id); // <--- Chama o repositório
+    const movie = await this.repository.findById(id); // Chama o repositório
 
     if (!movie) throw new NotFoundError("Filme não encontrado");
-    // 3. Aplicação dos seus cenários de Gherkin (Fallback de Título e N/A)
     return {
       id: movie.id,
-      title: movie.title || "No title", // Scenario: Ausência de título
-      synopsis: movie.synopsis || "N/A",      // Scenario: Ausência de metadados
+      title: movie.title || "No title",
+      synopsis: movie.synopsis || "N/A",
       genres: movie.genres || "N/A",
       duration: movie.duration || "N/A",
       director: movie.director || "N/A",
