abc2f9cb618dcabf9d7c480830e88a70354c6d0e
njrsouza <njrsouza@outlook.com>
2026-05-18 19:00:18 -0300

    feat(movie-routes): add admin verification to movie routes

--- backend/src/routes/movie-routes.ts
+++ backend/src/routes/movie-routes.ts
@@ -5,10 +5,12 @@ import {
   deleteMovie,
   patchMovie,
 } from "../controllers/movie-controller";
+import { verificarAdmin } from "../middlewares/auth-middleware";
 
 export const router = Router();
 
-router.post("/movies", postMovie);
 router.get("/movies", getMovies);
-router.patch("/movies/:id", patchMovie);
-router.delete("/movies/:id", deleteMovie);
+
+router.post("/movies", verificarAdmin, postMovie);
+router.patch("/movies/:id", verificarAdmin, patchMovie);
+router.delete("/movies/:id", verificarAdmin, deleteMovie);
