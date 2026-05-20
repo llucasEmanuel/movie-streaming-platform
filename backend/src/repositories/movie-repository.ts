b915689a472852080c2bb841cf18c9cc3ecf65f1
Juliana Silva <juliluiza2013@gmail.com>
2026-05-20 00:48:52 +0000

    "chore: merge develop branch into local"

--- backend/src/repositories/movie-repository.ts
+++ backend/src/repositories/movie-repository.ts
@@ -1,5 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-import { prisma as defaultPrisma } from "../database/prisma-client";
 import { MovieModel } from "../models/movie-model";
 import { PrismaClient } from "../generated/prisma";
 import { prisma } from "../database/prisma-client"; // <-- Única importação do prisma
